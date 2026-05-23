88ada8966305ccf6b5347a69cd1e434965c19777
Andrew Zhang <andrewzhang927@gmail.com>
2026-05-22 22:15:13 -0400

    upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://sts2potions.andrew-zhang.ca"
 });
